72f7b17da968fba0e8aeec510d9ce5262b651fca
DannLarnes <matiasegarcia96@gmail.com>
2017-05-22 15:04:29 -0300

    Add files via upload

---
+++ TP3.py
@@ -0,0 +1,19 @@
+import pyaudio
+import soundplayer
+DICC-COMANDOS:{
+LOAD: cargarcancion(),
+STORE: guardarcancion(),
+STEP:'...',
+BACK:'...',
+TRACKADD:'...',
+TRACKDEL:'...',
+MARKADD:'...',
+MARKADDNEXT:'...',
+MARKADDPREV:'...',
+TRACKON:'...',
+TRACKOFF:'...',
+PLAY:'...',
+PLAYALL:'...',
+PLAYMARKS:'...',
+PLAYSECONDS:'...',
+}
