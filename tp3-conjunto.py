64dbdd2002d00681602e31d076cbbce61a4e7b52
andzcs <andresgonzalezfiuba@hotmail.com>
2017-06-02 17:16:31 -0300

    Update tp3-conjunto.py

--- tp3-conjunto.py
+++ tp3-conjunto.py
@@ -1,8 +1,8 @@
-Import import soundPlayer as pysounds
+import soundPlayer as pysounds
 
 FUN_SOUND={'SQ':pysounds.SoundFactory.get_square_sound,
-'TRIA':pysounds.SoundFactory.get_triangular_sound
-'SINE':pysounds.SoundFactory.get_sine_sound
+'TRIA':pysounds.SoundFactory.get_triangular_sound,
+'SINE':pysounds.SoundFactory.get_sine_sound,
 'NOISE':pysounds.SoundFactory.get_noise_sound}
 
 ###################
@@ -47,6 +47,12 @@ class ListaEnlazada:
 	def __init__(self):
 		self.prim = None
 		self.len = 0
+
+	def __str__(self):
+		lista = []
+		for i in self:
+			lista.append(i)
+		return str((lista))
 	
 	def pop(self, i=None ):
 		if i is None:
@@ -155,20 +161,20 @@ class _IteradorLE:
     
     def __init__(self,prim):
         self.actual=prim
-        self.anterior=Pila()
+        self.ant=Pila()
         
     def __next__(self):
         if not self.actual:
             raise StopIteration
         dato=self.actual.dato
-        self.anterior.apilar(self.actual)
+        self.ant.apilar(self.actual)
         self.actual=self.actual.prox
         return dato
 
     def anterior(self):
-    	if not self.anterior:
+    	if not self.ant:
     		raise StopIteration
-    	nodo=self.anterior.desapilar()
+    	nodo=self.ant.desapilar()
     	self.actual=nodo
     	return nodo.dato
 
@@ -176,6 +182,7 @@ class _IteradorLE:
     	nodo=_Nodo(elem)
     	nodo.prox=self.actual.prox
     	self.actual.prox=nodo
+    	self.__next__()
 
 
 
@@ -195,6 +202,9 @@ class _Marca:
 	def modificar_audio(self,posicion,x):
 		self.audio[posicion] = x
 
+	def __str__(self):
+		return str("audio {}, tiempo {}".format(self.audio,self.tiempo))
+
 class _Track:
 
 	def __init__(self,funcion,frecuencia,volumen):
@@ -202,19 +212,24 @@ class _Track:
 		self.frec=frecuencia
 		self.vol=volumen
 
+	def __str__(self):
+		return str("funcion {}, frecuencia {}, volumen {}".format(self.fun,self.frec,self.vol))
+
 class Cancion:
 	def __init__(self):
 		self.tracks =[]
 		self.marcas = ListaEnlazada()
-		self.pos = self.marcas.__iter__()
+		self.pos = None
 
+	def __str__(self):
+		return ("tracks {}, marca {}, posicion {}".format(self.tracks,self.marcas,self.pos))
 
 	def agregar_track(self,track,posicion=None):
 		n_track=_Track(track[0],int(track[1]),float(track[2]))
 		if posicion is None:
-			self.tracks.append(track)
+			self.tracks.append(n_track)
 		else:
-			self.tracks.insert(posicion,track)
+			self.tracks.insert(posicion,n_track)
 		for marca in self.marcas:
 				marca.agregar_audio('.',posicion)
 
@@ -228,11 +243,15 @@ class Cancion:
 		if antosig=='ant':
 			mov_atras()
 		elif antosig=='sig':
-			mov_adelante():
-		self.pos.iter_insertar(self.pos,Nuevo)
+			mov_adelante()
+		if self.marcas.prim == None:
+			self.marcas.append(Nuevo)
+			self.pos = self.marcas.__iter__()
+		else:		
+			self.pos.iter_insertar(Nuevo)
 
 	def trackonoff(self,n,onoff):
-		if onoff==on:
+		if onoff == "on":
 			self.pos.__next__().modificar_audio(n,'#')
 		else:
 			self.pos.__next__().modificar_audio(n,'.')
@@ -243,50 +262,58 @@ class Cancion:
 			while parametro:
 				self.pos.__next__()
 				parametro -=1
-		except StopIteration
+		except StopIteration:
+			return
 
 	def mov_atras(self,parametro=1):
 		try:
 			while parametro:
 				self.pos.anterior()
-		except StopIteration
+		except StopIteration:
+			return	
 
 		
 class Reproductor:
 
-	def __init__(self,):
+	def __init__(self):
 		self.cancion=None
 		self.sonidos=[]
-		self.canales=len(self.cancion.tracks)
-		self.cursor=self.cancion.pos
+		self.cursor=None
 		self.nom_cancion=None
 
+	def __str__(self):
+		return str("cancion {}, sonidos {}, cursor {}, nombre_cacion {}".format(self.cancion,self.sonidos,self.cursor,self.nom_cancion))
+
 	def reiniciar_cursor(self):
-		while self.cursor.anterior:
-			self.cursor.anterior()
+		try:
+			while True:
+				self.cursor.anterior()
+		except Exception:
+			 return
 
 	def cargar_de_archivo(self,archivoplp):
 		cancionarch=Cancion()
 		with open(archivoplp,'r') as archivo:
 			archivo.readline()#salteo el header del archivo
 			for linea in archivo:
-			line=linea.rstrip('\n')
-			field , data = line.split()
-			if field== 'C':
-				continue
-			elif field == 'S':
-				track=data.split('|')
-				cancionarch.agregar_track(track)
-			elif field =='T':
-				tiempo = float(data)
-			elif field == 'N':
-				sonidos=data.split('')
-				cancionarch.agregar_marca(tiempo)
-				for i in sonidos:
-					if sonidos[i]=='#':
-						cancionarch.trackonoff(i,on)
+				line = linea.rstrip('\n')
+				field , data = line.split(",")
+				if field == 'C':
+					continue
+				elif field == 'S':
+					track=data.split('|')
+					cancionarch.agregar_track(track)
+				elif field =='T':
+					tiempo = float(data)
+				elif field == 'N':
+					sonidos=data
+					cancionarch.agregar_marca(tiempo)
+					for i in range (len(sonidos)):
+						if sonidos[i] == '#':
+							cancionarch.trackonoff(i,"on")
 		self.nom_cancion=archivoplp
 		self.cancion=cancionarch
+		self.cursor = self.cancion.pos
 
 	def guardar_archivo(self,nombre=None):
 		if not nombre:
@@ -310,63 +337,71 @@ class Reproductor:
 
 
 	def crear_sonidos(self):
-		for i,track in self.cancion.tracks:
+		for i,track in enumerate(self.cancion.tracks):
 			if 'SQ' in track.fun:
-				func=FUN_SOUND[SQ]
+				func=FUN_SOUND["SQ"]
 				A=int(track.fun.strip('SQ'))/100#parametro de sonido 'square' no se que signifique
-				i=func(int(track.frec),float(track.vol),A)
+				i=func(float(track.frec),float(track.vol),A)
 			else:
 				func=FUN_SOUND[track.fun]
-				i=func(int(track.frec),float(track.vol))
+				i=func(float(track.frec),float(track.vol))
 			self.sonidos.append(i)
 
 	def reproducir(self,cuanto=None):
 		self.crear_sonidos()
-		sp = pysounds.SoundPlayer(self.canales)
+		sp = pysounds.SoundPlayer(len(self.sonidos))
 		if not cuanto:
 			marca=self.cursor.__next__()
 			sonidos=[]
 			for i in len(marca.audio):
 				if marca.audio[i]=='#':
-					sonidos.append(i)
+					sonidos.append(self.sonidos[i])
 			sp.play_sounds(sonidos,marca.tiempo)
+			self.cursor.anterior()
 
-		elif cuanto==all:
-			while self.cursor.anterior:
-				self.cursor.anterior()
-			while self.cursor:
-				marca=self.cursor.__next__()
-				sonidos=[]
-				for i in len(marca.audio):
-					if marca.audio[i]=='#':
-						sonidos.append(i)
-				sp.play_sounds(sonidos, marca.tiempo)
-		elif cuanto[0]==marcas:
+		elif cuanto=="all":
+			self.reiniciar_cursor()
+			try:
+				while self.cursor:
+					marca=self.cursor.__next__()
+					sonidos=[]
+					for i in range (len(marca.audio)):
+						if marca.audio[i]=='#':
+							sonidos.append(self.sonidos[i])
+					sp.play_sounds(sonidos, marca.tiempo)
+			except StopIteration:
+				self.reiniciar_cursor()
+
+		elif cuanto[0] == "marcas":
 			acum=0
-			while self.cursor:
-				marca=self.cursor.__next__()
-				sonidos=[]
-				for i in len(marca.audio):
-					if marca.audio[i]=='#':
-						sonidos.append(i)
-				sp.play_sounds(sonidos, marca.tiempo)
-				acum+=1
-				if acum>=cuanto[1]
-		elif cuanto[0]==tiempo:
+			try:
+				while self.cursor:
+					marca=self.cursor.__next__()
+					sonidos=[]
+					for i in len(marca.audio):
+						if marca.audio[i]=='#':
+							sonidos.append(self.sonidos[i])
+					sp.play_sounds(sonidos, marca.tiempo)
+					acum += 1
+					if acum >= cuanto[1]:
+						break
+			except StopIteration:
+				while acum > 0:
+					acum -= 1
+					self.anterior()
+
+		elif cuanto[0] == "tiempo":
 			acum=0
-			while self.cursor:
-				marca=self.cursor.__next__()
-				sonidos=[]
-				for i in len(marca.audio):
-					if marca.audio[i]=='#':
-						sonidos.append(i)
-				sp.play_sounds(sonidos, marca.tiempo)
-				acum+=marca.tiempo
-				if acum>=cuanto[1]
-
-
-
-
-
-
-
+			try:
+				while self.cursor:
+					marca = self.cursor.__next__()
+					sonidos = []
+					for i in len(marca.audio):
+						if marca.audio[i] == '#':
+							sonidos.append(self.sonidos[i])
+					sp.play_sounds(sonidos, marca.tiempo)
+					acum += marca.tiempo
+					if acum >= cuanto[1]:
+						break
+			except StopIteration:
+				self.reiniciar_cursor()
